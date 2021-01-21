68719f3cb5f572bd4aece86eb045ecc31588291f
mopsiq <60403947+mopsiq@users.noreply.github.com>
2021-01-22 01:10:58 +0200

    Reform Alpha

--- app.js
+++ app.js
@@ -1,76 +1,136 @@
 'use strict';
 
+const seatsInHall = {
 
-const btnForBlock = document.querySelectorAll('.block__btn');
-const modalForBlock = document.querySelectorAll('.block__modal');
-const cellInTable = document.querySelectorAll('.block__cell');
-const wrapper = document.querySelector('.wrapper');
+  'date': {
+      1: {
 
-let obj = {
- 'table1': {
-    cells: [1, 4, 12, 15, 19, 37, 38],
- },
- 'table2': {
-    cells: [2, 5, 7],
- },
+      }
+  },
+
+  'films': {
+    'Batman': {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+    'Green Tree': {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+    'Gag': {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+    'Agent K': {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+    'Feed': {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+  },
+
+  'hall': {
+    1: {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+    2: {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+    3: {
+      reservedSets: [],
+      reservedSetsUser: [],
+    },
+  }
+
 };
 
 
-btnForBlock.forEach(function(item) {
+document.addEventListener('DOMContentLoaded', () => {
   
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
 
-    modalForBlock.forEach((currentModal) => {
-        if(currentModal.getAttribute('data-value') === item.getAttribute('data-value')) {
-            currentModal.classList.add('active');
-            wrapper.classList.toggle('active');
-            fillingInCells(currentModal, obj);
 
-            wrapper.addEventListener('click', () => {
-              wrapper.classList.remove('active');
-              currentModal.classList.remove('active');
-            });
-        };
-        
-    });
-  
-  });
-  
+
 });
 
-function fillingInCells(poster, object) {
-  let currentTable = 'table' + poster.getAttribute('data-value');
-  let blockInPlaces = poster.lastElementChild;
-  let reservedCells = [];
-
-  for(let i = 0; i < blockInPlaces.childElementCount; i++) {
-      let currentCell = blockInPlaces.children[i];
-
-      if(object[currentTable].cells.includes(i + 1)) {
-        currentCell.classList.add('active')
-      };
-
-      currentCell.addEventListener('click', () => {
-
-        if(currentCell.classList.contains('active')) {
-          return false;
-        }
-
-        currentCell.classList.toggle('reservation')
-        if(currentCell.classList.contains('reservation')) {
-            reservedCells.push(i + 1);
-        } else {
-            reservedCells.splice(reservedCells.indexOf(i + 1), 1);
-        };
-
-      });
-
-      
-  };
 
 
-};
+
+
+
+
+
+
+
+
+
+
+
+// function fillingInCells(poster, object) {
+//   let a = poster.children;
+//   for(let i = 0; i < poster.children.length; i++) {
+//     for(let j = 0; j < poster.children[i].children.length; j++) {
+//       console.log(poster.children[i].children[j])
+//     }
+//   }
+
+// }
+
+
+// function fillingInCells(poster, object) {
+//   let currentTable = 'table' + poster.getAttribute('data-value');
+//   let blockInPlaces = poster.children[1];
+//   let reservedCells = [];
+
+//   console.log(object[currentTable].cells)
+//   for(let i = 0; i < blockInPlaces.childElementCount; i++) {
+//       let currentCell = blockInPlaces.children[i];
+
+//       if(object[currentTable].cells.includes(i + 1)) {
+//         currentCell.classList.add('active')
+//       };
+
+//       if(object[currentTable].reservedUser.includes(i + 1)) {
+//         currentCell.classList.add('reserved')
+//       }
+
+//           currentCell.addEventListener('click', () => {
+
+//             if(currentCell.classList.contains('active')) {
+//               return false;
+//             }
+
+//             currentCell.classList.toggle('reserved')
+//             if(currentCell.classList.contains('reserved')) {
+//                 reservedCells.push(i + 1);
+//                 // currentCell.classList.add('active')
+//             } else {
+//                 reservedCells.splice(reservedCells.indexOf(i + 1), 1);
+//             };
+//             console.log(reservedCells)
+//           });
+
+//   };
+
+// function test(model, object, table) {
+//   // for(let i = 0; i < model.length; i++) {
+//   //   object[table].reservedUser.push(model[i])
+//   // }
+//   while(model.length !== 0) {
+//     let i = 0;
+//     object[table].reservedUser.push(model[i])
+//     model.pop()
+//     i++;
+//   }
+//   console.log(object[table].reservedUser)
+//   console.log(model)
+//   console.log(object[table])
+// }
+
+
 
 
 
