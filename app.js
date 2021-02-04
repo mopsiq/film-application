c19212025fb0a6208408c58af1d947f318dd404f
mopsiq <60403947+mopsiq@users.noreply.github.com>
2021-02-04 15:23:07 +0200

    bags fixed

--- app.js
+++ app.js
@@ -7,47 +7,41 @@ const seats = document.querySelectorAll('.seats');
 
 const dateSelected = document.getElementById('date');
 const timeSelected = document.getElementById('time');
-console.log(dateSelected)
+
 
 let objectInDates = {
 
   dates: {
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
+    0: '1',
+    1: '2',
+    2: '3',
+    3: '4',
+    4: '5',
+    5: '6',
+    6: '7',
   },
 
 };
 
 let users = {};
 let reservedForUser = [];
-
 let JSONInLocal;
-let testDate;
 
 
 function generateDate(object) {
   let newWeekDate = new Date(2021, 0, 25);
-  let currentDate = new Date();
-
-  if(currentDate.getDay() === 1) {
-    newWeekDate = new Date(currentDate);
-  }
+  // let currentDate = new Date();
 
   let size = Object.keys(object.dates).length;
 
   let month = newWeekDate.getMonth() + 1;
   month < 10 ? month = '0' + month : false;
 
-  let dateInDay = newWeekDate.getDate();
+  let dateInDay = newWeekDate.getDate();  
   let year = newWeekDate.getFullYear();
 
 
-  for(let i = 0; i <= size; i++) {
+  for(let i = 0; i < size; i++) {
     object.dates[i] = `${year}:${month}:${dateInDay + i}`
   }
 
@@ -69,7 +63,7 @@ function checkingInReserv(seatsInBlock, localStore) {
           seatsInBlock[i].classList.remove('reserved')
         }
 
-        seatsInBlock[i].classList.add('active')
+        // seatsInBlock[i].classList.add('active')
     }
   }
     
@@ -86,102 +80,90 @@ function creatingObjectInSeats(object, date, time) {
      
 };
 
-function generateSeatsInRange(currentSelect, seatsBlock, object) {
+function generateSeatsInRange(seatsBlock, object) {
 
   seatsBlock.forEach((item) => {
-
+    
     item.classList.remove('reserved')
     item.classList.remove('clicked')
 
-    if(users?.[dateSelected.value]?.[timeSelected.value]) {
-      let JSONParse = JSON.parse(users[dateSelected.value][timeSelected.value])
+      if(object?.[dateSelected.value]?.[timeSelected.value]) {
+        btnForBlock.setAttribute('disabled', 'disabled')
+        let JSONParse = JSON.parse(object[dateSelected.value][timeSelected.value])
 
-      if(JSONParse.includes(+item.getAttribute('seat-value'))) {
-        item.classList.add('reserved')
+        if(JSONParse.includes(+item.getAttribute('seat-value'))) {
+          item.classList.add('reserved')
+        } else {
+          item.classList.remove('reserved')
+        }
+        // item.classList.add('active')
       } else {
-        item.classList.remove('reserved')
-      }
+        btnForBlock.removeAttribute('disabled')
+      };
 
-    }
-      
-  })
+  });
 
-}
+};
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  for(let i = 1; i < dateSelected.childElementCount; i++) {
+    dateSelected[i].textContent = objectInDates.dates[i - 1]
+  };
   
   if(window.localStorage.getItem('localObject') !== null || undefined) {
     users = JSON.parse(window.localStorage.getItem('localObject'));
-  }
+  };
   
   // localStorage.clear();
   console.log(users)
-  // checkingInReserv(seats, window.localStorage.getItem('localReserveds'));
 
       selectInBlock.forEach((item) => {
         item.addEventListener('change', () => {
-          generateSeatsInRange(item, seats, users)
-        });
-
-        if(item.getAttribute('name') === 'date') {
-      
-          for(let i = 0; i < item.childElementCount; i++) {
-              item.children[i].textContent = objectInDates.dates[i - 1];
-              item.children[0].textContent = 'Выбор даты'
-          };
-      
-        };
-      
+          generateSeatsInRange(seats, users)
+        });  
       });
     
-    seats.forEach((item) => {
+      seats.forEach((item) => {
+        item.addEventListener('click', () => {
+            let currentItem = +item.getAttribute('seat-value');
 
-      if(item.classList.contains('reserved')) {
-        reservedForUser.push(+item.getAttribute('seat-value'))
-        return false;
-      }
+              // if(item.classList.contains('active')) {
+              //   return false
+              // }
 
-      item.addEventListener('click', () => {
-          let currentItem = +item.getAttribute('seat-value');
+              if(reservedForUser.length >= 8) {
+                console.log('Бронирование невозможно')
+              } else {
+                item.classList.toggle('clicked');
 
-            if(reservedForUser.length >= 31) {
-              console.log('Бронирование невозможно')
-            } else {
-              item.classList.toggle('clicked');
+              if(item.classList.contains('clicked')) {
+                reservedForUser.push(currentItem);
+              } else {
+                reservedForUser.splice(reservedForUser.indexOf(currentItem), 1);
+              };
 
-            if(item.classList.contains('clicked')) {
-              reservedForUser.push(currentItem);
-            } else {
-              reservedForUser.splice(reservedForUser.indexOf(currentItem), 1);
-            };
-
-            if(reservedForUser.length === 0) {
-              btnForBlock.setAttribute('disabled', 'disabled')
-            } else {
-              btnForBlock.removeAttribute('disabled')
             }
-          }
 
-          console.log(reservedForUser)
+            console.log(reservedForUser)
+        });
+
       });
 
-    });
 
 
+      btnForBlock.addEventListener('click', () => {
+          JSONInLocal = JSON.stringify(reservedForUser);
+          window.localStorage.setItem('localReserveds', JSONInLocal);
 
-    btnForBlock.addEventListener('click', () => {
-        JSONInLocal = JSON.stringify(reservedForUser);
-        window.localStorage.setItem('localReserveds', JSONInLocal);
+          checkingInReserv(seats, window.localStorage.getItem('localReserveds'));
+          creatingObjectInSeats(users, dateSelected, timeSelected)
 
-        checkingInReserv(seats, window.localStorage.getItem('localReserveds'));
-        creatingObjectInSeats(users, selectInBlock[0], selectInBlock[1])
-
-        window.localStorage.setItem('localObject', JSON.stringify(users, null, 2));
-        console.log(window.localStorage.getItem('localObject'))
-    });
+          window.localStorage.setItem('localObject', JSON.stringify(users, null, 2));
+          console.log(window.localStorage.getItem('localObject'))
+      });
 
 });
 
